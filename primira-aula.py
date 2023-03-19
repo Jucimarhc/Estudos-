e24817803ec07ece616543810484389f1a581c01
jucimar conceição de oliveira <99962073+Jucimarhc@users.noreply.github.com>
2023-03-19 20:33:42 -0300

    atualização do codigo ,para crianção de arquivo com login e senha

--- primira-aula.py
+++ primira-aula.py
@@ -1,15 +1,17 @@
-f = open("bashrc","a")
+
+""" f = open("bashrc","a")
 f.write("\n primeira aula de phyton ")
 f.close()
 
 
 f= open("bashrc")
 arquivo = f.read()
-print(arquivo)
+print(arquivo) """
 
 
-
-
-
-
-
+nome = input("Digite seu Login:")
+senha = input("Digite sua senha :")
+f = open("Login é senha.txt,","a") 
+f.write ("Login :{}\n".format(nome) + "Senha:{}\n".format(senha))
+f.close()
+print("Nome e senha do usuário foram salvos no arquivo.")
