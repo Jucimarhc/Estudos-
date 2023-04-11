b109b3a50b46fbd91adff6be6b03431fe886f4af
jucimar conceição de oliveira <jucimarconceicao2013@gmail.com>
2023-04-11 09:43:25 -0300

    Codigo para criação de arquivo , diretorio é mudança de diretorio

--- primira-aula.py
+++ primira-aula.py
@@ -15,3 +15,28 @@ f = open("Login é senha.txt,","a")
 f.write ("Login :{}\n".format(nome) + "Senha:{}\n".format(senha))
 f.close()
 print("Nome e senha do usuário foram salvos no arquivo.")"""
+ 
+import os
+
+try :
+    f = open("Login_senha.txt","a").close()
+
+except OSError as erro:
+    print("erro ao criar o arquivo ")
+    print("Descrição", erro)
+else:
+    print("arquivo Criado")    
+
+try:
+    os.mkdir('Login')
+except FileExistsError as erro:
+    print("Diretoria ja existe \n", erro)
+except PermissionError as erro: 
+    print("Vocé não possue privilegios")
+    print("Descrição",erro)    
+    
+try:
+    os.replace("Login_senha.txt","Login/Login_senha.txt")
+except FileNotFoundError as erro:
+    print("Arquivo não Existe\n")
+    print("Descrição", erro)
